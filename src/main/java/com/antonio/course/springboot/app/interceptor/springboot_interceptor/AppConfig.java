ba5fc47a4a756ef05fa6669449eb450d143583b7
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2024-12-26 10:32:18 -0300

    Add LoadingTimeInterceptor and AppConfig for request timing logging

---
+++ src/main/java/com/antonio/course/springboot/app/interceptor/springboot_interceptor/AppConfig.java
@@ -0,0 +1,23 @@
+package com.antonio.course.springboot.app.interceptor.springboot_interceptor;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+  @Autowired
+  @Qualifier("loadingTimeInterceptor")
+  private HandlerInterceptor timeInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(timeInterceptor);
+  }
+
+}
--- src/main/java/com/antonio/course/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/antonio/course/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,31 +1,29 @@
 package com.antonio.course.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.micrometer.common.lang.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("loadingTimeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+  private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-      ModelAndView modelAndView) throws Exception {
-    long startTime = (Long) request.getAttribute("startTime");
-    long endTime = System.currentTimeMillis();
-    long executeTime = endTime - startTime;
-
-    if (modelAndView != null) {
-      modelAndView.addObject("executeTime", executeTime);
-    }
+      @Nullable ModelAndView modelAndView) throws Exception {
+    logger.info("LoadingTimeInterceptor: postHandle() enter");
   }
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    long startTime = System.currentTimeMillis();
-    request.setAttribute("startTime", startTime);
+    logger.info("LoadingTimeInterceptor: preHandle() enter");
     return true;
   }
 
